7e0df0e765a5b8b227f44cd3de4de996067f5c7d
sunxl8 <sunxl8@centaline.com.cn>
2017-02-10 13:55:55 +0800

    create

---
+++ app/src/main/java/sunxl8/your_diary/Constant/Constant.java
@@ -0,0 +1,8 @@
+package sunxl8.your_diary.constant;
+
+/**
+ * Created by sunxl8 on 2017/2/10.
+ */
+
+public class Constant {
+}
---
+++ app/src/main/java/sunxl8/your_diary/ui/login/LoginPresenter.java
@@ -0,0 +1,15 @@
+package sunxl8.your_diary.ui.login;
+
+import sunxl8.your_diary.base.BaseActivity;
+import sunxl8.your_diary.base.BasePresenter;
+
+/**
+ * Created by sunxl8 on 2017/2/10.
+ */
+
+public class LoginPresenter extends BasePresenter<LoginContract.View> implements LoginContract.Presenter {
+
+    protected LoginPresenter(BaseActivity activity) {
+        super(activity);
+    }
+}
