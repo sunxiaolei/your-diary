a0a521b3142c1037be088bb9542dd472fee0488a
sunxiaolei <xiaoleisun92@gmail.com>
2017-02-10 13:55:55 +0800

    create

---
+++ app/src/main/java/sunxl8/your_diary/ui/login/LoginActivity.java
@@ -0,0 +1,8 @@
+package sunxl8.your_diary.ui.login;
+
+/**
+ * Created by sunxl8 on 2017/2/10.
+ */
+
+public class LoginActivity {
+}
---
+++ app/src/main/java/sunxl8/your_diary/ui/login/LoginContract.java
@@ -0,0 +1,19 @@
+package sunxl8.your_diary.ui.login;
+
+import sunxl8.your_diary.base.IPresenter;
+import sunxl8.your_diary.base.IView;
+
+/**
+ * Created by sunxl8 on 2017/2/10.
+ */
+
+public interface LoginContract {
+
+    interface View extends IView {
+
+    }
+
+    interface Presenter extends IPresenter<View> {
+
+    }
+}
