21c83077b7050375d1168fff383e528a81601ab6
sunxl8 <sunxl8@centaline.com.cn>
2017-02-21 15:31:50 +0800

    数据库操作 统一管理

--- app/src/main/java/sunxl8/your_diary/ui/diary/edit/DiaryEditPresenter.java
+++ app/src/main/java/sunxl8/your_diary/ui/diary/edit/DiaryEditPresenter.java
@@ -1,20 +1,9 @@
 package sunxl8.your_diary.ui.diary.edit;
 
-import org.greenrobot.greendao.query.Query;
-
-import java.util.List;
-
 import sunxl8.your_diary.base.BaseActivity;
-import sunxl8.your_diary.base.BaseApplication;
 import sunxl8.your_diary.base.BasePresenter;
-import sunxl8.your_diary.db.dao.DaoSession;
-import sunxl8.your_diary.db.dao.DiaryEntityDao;
-import sunxl8.your_diary.db.dao.ItemEntityDao;
+import sunxl8.your_diary.db.DbManager;
 import sunxl8.your_diary.db.entity.DiaryEntity;
-import sunxl8.your_diary.db.entity.ItemEntity;
-import sunxl8.your_diary.event.MainRefreshEvent;
-import sunxl8.your_diary.ui.diary.calendar.DiaryCalendarContract;
-import sunxl8.your_diary.util.RxBus;
 
 /**
  * Created by sunxl8 on 2017/2/13.
@@ -22,32 +11,15 @@ import sunxl8.your_diary.util.RxBus;
 
 public class DiaryEditPresenter extends BasePresenter<DiaryEditContract.View> implements DiaryEditContract.Presenter {
 
-    private DiaryEntityDao mEntityDao;
-    private ItemEntityDao entityItemDao;
-
     protected DiaryEditPresenter(BaseActivity activity) {
         super(activity);
-        DaoSession daoSession = ((BaseApplication) activity.getApplication()).getDaoSession();
-        mEntityDao = daoSession.getDiaryEntityDao();
-        entityItemDao = daoSession.getItemEntityDao();
     }
 
-    private void editItemEntityCount(Long diaryId) {
-        Query query = mEntityDao.queryBuilder()
-                .where(DiaryEntityDao.Properties.DiaryId.eq(diaryId))
-                .orderDesc(DiaryEntityDao.Properties.Date)
-                .build();
-        ItemEntity entity = entityItemDao.load(diaryId);
-        entity.setItemCount(query.list().size());
-        entityItemDao.update(entity);
-        RxBus.getInstance()
-                .post(new MainRefreshEvent());
-    }
 
     @Override
     public void save(Long diaryId, DiaryEntity entity) {
-        mEntityDao.save(entity);
-        editItemEntityCount(diaryId);
+        DbManager.addDiary(entity);
+        DbManager.updateItemCountByItemId(diaryId);
         mView.saveDone();
     }
 }
