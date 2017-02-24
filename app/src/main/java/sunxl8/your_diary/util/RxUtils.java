d5af5529b09781cd2896c6839c41a643058cee4c
sunxiaolei <xiaoleisun92@gmail.com>
2017-02-24 16:29:41 +0800

    rxutil

---
+++ app/src/main/java/sunxl8/your_diary/util/RxUtils.java
@@ -0,0 +1,24 @@
+package sunxl8.your_diary.util;
+
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+public class RxUtils {
+
+    /**
+     * 统一线程处理
+     * @param <T>
+     * @return
+     */
+    public static <T> Observable.Transformer<T, T> applyIoSchedulers() {    //compose简化线程
+        return new Observable.Transformer<T, T>() {
+            @Override
+            public Observable<T> call(Observable<T> observable) {
+                return observable.subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+}
